e80f32701f59538c654e48ae533c8d8c0c700bc9
Daniel Moreto <dfmoreto@gmail.com>
2018-12-01 23:25:39 -0200

    Configure Devise and Simple Token Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope :api do
+    scope :v1, defaults: { format: :json } do
+      devise_for :users
+    end
+  end
 end
