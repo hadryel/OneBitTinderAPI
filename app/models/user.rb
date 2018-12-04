904efb1fc36fa69777ddbfafdfd416ce021cb5e3
Daniel Moreto <dfmoreto@gmail.com>
2018-12-04 20:56:52 -0200

    Add method default_photo at User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,11 @@ class User < ApplicationRecord
   has_many :likes_given, class_name: "Like", foreign_key: "liker_id"
   has_many :likes_earned, class_name: "Like", foreign_key: "likee_id"
   
+
+  def default_photo
+    self.photos.where(default: true).take
+  end
+
   
   def matches
     Match.where('matcher_id = :user OR matchee_id = :user', user: self.id)
