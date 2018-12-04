483816468fd1b16ec4439c1942b3cf70296825c2
Daniel Moreto <dfmoreto@gmail.com>
2018-12-04 08:13:53 -0200

    Create controlelr for Like with :create action

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :photos
+      resources :users, only: [] do
+        resources :like, as: :likes, only: :create
+      end
     end
   end
 end
