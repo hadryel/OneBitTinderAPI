874ecbc535218fc115d34f1da9fbba99b450e80e
Daniel Moreto <dfmoreto@gmail.com>
2018-12-04 20:57:17 -0200

    Add workaround on Like to skip presence validation when :liked is false

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
   belongs_to :liker, class_name: "User"
   belongs_to :likee, class_name: "User"
 
-  validates :liked, presence: true
+  validates :liked, presence: true, if: ->(like) { like.liked.nil? }
   validates :liker, uniqueness: { scope: :likee_id, message: "can't perform a like/unlike to a Likee twice" }
 
   after_create :check_and_create_match, if: ->(like) { like.liked? }
