8fb78ace573ac4cd39e11963971989bedac2aff7
Daniel Moreto <dfmoreto@gmail.com>
2018-12-04 22:19:03 -0200

    Add association with :messages at model Match

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,6 +3,7 @@ class Match < ApplicationRecord
   
   belongs_to :matcher, class_name: "User"
   belongs_to :matchee, class_name: "User"
+  has_many :messages
   
   validates_with MatchWithSameUsersValidator
 
