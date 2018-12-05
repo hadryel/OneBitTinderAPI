e229a130ddcb0a3e6c55dbac03c5aafecd6041f6
Daniel Moreto <dfmoreto@gmail.com>
2018-12-04 22:18:33 -0200

    Create model Message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_214519) do
+ActiveRecord::Schema.define(version: 2018_12_04_235803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,16 @@ ActiveRecord::Schema.define(version: 2018_12_04_214519) do
     t.index ["matcher_id"], name: "index_matches_on_matcher_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "match_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["match_id"], name: "index_messages_on_match_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "photos", force: :cascade do |t|
     t.boolean "default", default: false
     t.bigint "user_id"
@@ -84,5 +94,7 @@ ActiveRecord::Schema.define(version: 2018_12_04_214519) do
   add_foreign_key "likes", "users", column: "liker_id"
   add_foreign_key "matches", "users", column: "matchee_id"
   add_foreign_key "matches", "users", column: "matcher_id"
+  add_foreign_key "messages", "matches"
+  add_foreign_key "messages", "users"
   add_foreign_key "photos", "users"
 end
