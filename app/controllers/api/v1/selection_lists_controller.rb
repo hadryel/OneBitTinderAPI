8e0ad84723b61cd73f76f17db90662d2b61c872d
Daniel Moreto <dfmoreto@gmail.com>
2018-12-29 11:27:03 -0200

    Remove byebug from controller

--- app/controllers/api/v1/selection_lists_controller.rb
+++ app/controllers/api/v1/selection_lists_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::SelectionListsController < ApplicationController
   def index
     @users = SelectionListService.call(current_user, page: params[:page])
-    byebug
     render 'api/v1/users/index'
   end
 end
