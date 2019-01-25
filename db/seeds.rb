d84123bf172d032d5c52b0e24ff813f930e9f903
Daniel Moreto <dfmoreto@gmail.com>
2019-01-25 13:05:38 -0200

    Add description at seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,18 @@ ActiveRecord::Base.transaction do
   # DOGS
 
   beagle = User.create(name: "Beagle", email: "beagle@dog.com", password: "123456", password_confirmation: "123456",
-                      company: Faker::Company.name, college: Faker::Science.scientist)
+                      company: Faker::Company.name, college: Faker::Science.scientist, description: Faker::Lorem.sentence(3))
   beagle.photos.create
   beagle.photos.first.file.attach(io: File.open("#{photo_dir}/beagle_01.jpg"), filename: "beagle_01.jpg")
 
   buldogue = User.create(name: "Buldogue", email: "buldogue@dog.com", password: "123456", password_confirmation: "123456",
-                        company: Faker::Company.name, college: Faker::Science.scientist)
+                        company: Faker::Company.name, college: Faker::Science.scientist, description: Faker::Lorem.sentence(3))
   2.times { buldogue.photos.create }
   buldogue.photos.first.file.attach(io: File.open("#{photo_dir}/buldogue_01.jpg"), filename: "buldogue_01.jpg")
   buldogue.photos.second.file.attach(io: File.open("#{photo_dir}/buldogue_02.jpg"), filename: "buldogue_02.jpg")
 
   doberman = User.create(name: "Doberman", email: "doberman@dog.com", password: "123456", password_confirmation: "123456",
-                        company: Faker::Company.name, college: Faker::Science.scientist)
+                        company: Faker::Company.name, college: Faker::Science.scientist, description: Faker::Lorem.sentence(3))
   2.times { doberman.photos.create }
   doberman.photos.first.file.attach(io: File.open("#{photo_dir}/doberman_01.jpg"), filename: "doberman_01.jpg")
   doberman.photos.second.file.attach(io: File.open("#{photo_dir}/doberman_02.jpg"), filename: "doberman_02.jpg")
@@ -33,7 +33,7 @@ ActiveRecord::Base.transaction do
   labrador.photos.fourth.file.attach(io: File.open("#{photo_dir}/labrador_04.jpg"), filename: "labrador_04.jpg")
 
   poodle = User.create(name: "Poodle", email: "poodle@dog.com", password: "123456", password_confirmation: "123456",
-                      company: Faker::Company.name, college: Faker::Science.scientist)
+                      company: Faker::Company.name, college: Faker::Science.scientist, description: Faker::Lorem.sentence(3))
   2.times { poodle.photos.create }
   poodle.photos.first.file.attach(io: File.open("#{photo_dir}/poodle_01.jpg"), filename: "poodle_01.jpg")
   poodle.photos.second.file.attach(io: File.open("#{photo_dir}/poodle_02.jpg"), filename: "poodle_02.jpg")
@@ -50,18 +50,18 @@ ActiveRecord::Base.transaction do
   golden_retrivier.photos.first.file.attach(io: File.open("#{photo_dir}/golden_retrivier_01.jpg"), filename: "golden_retrivier_01.jpg")
   
   dachshund = User.create(name: "Dachshund", email: "dachshund@dog.com", password: "123456", password_confirmation: "123456",
-                          company: Faker::Company.name, college: Faker::Science.scientist)
+                          company: Faker::Company.name, college: Faker::Science.scientist, description: Faker::Lorem.sentence(3))
   dachshund.photos.create
   dachshund.photos.first.file.attach(io: File.open("#{photo_dir}/dachshund_01.jpg"), filename: "dachshund_01.jpg")
 
   chow_chow = User.create(name: "Chow Chow", email: "chow_chow@dog.com", password: "123456", password_confirmation: "123456",
-                          company: Faker::Company.name, college: Faker::Science.scientist)
+                          company: Faker::Company.name, college: Faker::Science.scientist, description: Faker::Lorem.sentence(3))
   2.times { chow_chow.photos.create }
   chow_chow.photos.first.file.attach(io: File.open("#{photo_dir}/chow_chow_01.jpg"), filename: "chow_chow_01.jpg")
   chow_chow.photos.second.file.attach(io: File.open("#{photo_dir}/chow_chow_02.jpg"), filename: "chow_chow_02.jpg")
 
   akita_inu = User.create(name: "Akita Inu", email: "akita_inu@dog.com", password: "123456", password_confirmation: "123456",
-                          company: Faker::Company.name, college: Faker::Science.scientist)
+                          company: Faker::Company.name, college: Faker::Science.scientist, description: Faker::Lorem.sentence(3))
   3.times { akita_inu.photos.create }
   akita_inu.photos.first.file.attach(io: File.open("#{photo_dir}/akita_inu_01.jpg"), filename: "akita_inu_01.jpg")
   akita_inu.photos.second.file.attach(io: File.open("#{photo_dir}/akita_inu_02.jpg"), filename: "akita_inu_02.jpg")
@@ -77,7 +77,7 @@ ActiveRecord::Base.transaction do
   scottish_fold.photos.second.file.attach(io: File.open("#{photo_dir}/scottish_fold_02.jpg"), filename: "scottish_fold_02.jpg")
 
   maine_coon = User.create(name: "Maine Coon", email: "maine_coon@cat.com", password: "123456", password_confirmation: "123456",
-                          company: Faker::Company.name, college: Faker::Science.scientist)
+                          company: Faker::Company.name, college: Faker::Science.scientist, description: Faker::Lorem.sentence(3))
   4.times { maine_coon.photos.create }
   maine_coon.photos.first.file.attach(io: File.open("#{photo_dir}/maine_coon_01.jpg"), filename: "maine_coon_01.jpg")
   maine_coon.photos.second.file.attach(io: File.open("#{photo_dir}/maine_coon_02.jpg"), filename: "maine_coon_02.jpg")
@@ -85,7 +85,7 @@ ActiveRecord::Base.transaction do
   maine_coon.photos.fourth.file.attach(io: File.open("#{photo_dir}/maine_coon_04.jpg"), filename: "maine_coon_04.jpg")
 
   chartreux = User.create(name: "Chartreux", email: "chartreux@cat.com", password: "123456", password_confirmation: "123456",
-                          company: Faker::Company.name, college: Faker::Science.scientist)
+                          company: Faker::Company.name, college: Faker::Science.scientist, description: Faker::Lorem.sentence(3))
   2.times { chartreux.photos.create }
   chartreux.photos.first.file.attach(io: File.open("#{photo_dir}/chartreux_01.jpg"), filename: "chartreux_01.jpg")
   chartreux.photos.second.file.attach(io: File.open("#{photo_dir}/chartreux_02.jpg"), filename: "chartreux_02.jpg")
@@ -96,7 +96,7 @@ ActiveRecord::Base.transaction do
   korat.photos.first.file.attach(io: File.open("#{photo_dir}/korat_01.jpg"), filename: "korat_01.jpg")
   
   turkish_van = User.create(name: "Turkish Van", email: "turkish_van@cat.com", password: "123456", password_confirmation: "123456",
-                            company: Faker::Company.name, college: Faker::Science.scientist)
+                            company: Faker::Company.name, college: Faker::Science.scientist, description: Faker::Lorem.sentence(3))
   3.times { turkish_van.photos.create }
   turkish_van.photos.first.file.attach(io: File.open("#{photo_dir}/turkish_van_01.jpg"), filename: "turkish_van_01.jpg")
   turkish_van.photos.second.file.attach(io: File.open("#{photo_dir}/turkish_van_02.jpg"), filename: "turkish_van_02.jpg")
@@ -117,7 +117,7 @@ ActiveRecord::Base.transaction do
   norwegian_forecast.photos.fifth.file.attach(io: File.open("#{photo_dir}/norwegian_forecast_05.jpg"), filename: "norwegian_forecast_05.jpg")
   
   ragdoll = User.create(name: "Ragdoll", email: "ragdoll@cat.com", password: "123456", password_confirmation: "123456",
-                        company: Faker::Company.name, college: Faker::Science.scientist)
+                        company: Faker::Company.name, college: Faker::Science.scientist, description: Faker::Lorem.sentence(3))
   ragdoll.photos.create
   ragdoll.photos.first.file.attach(io: File.open("#{photo_dir}/ragdoll_01.jpg"), filename: "ragdoll_01.jpg")
   
@@ -127,7 +127,7 @@ ActiveRecord::Base.transaction do
   russian_blue.photos.first.file.attach(io: File.open("#{photo_dir}/russian_blue_01.jpg"), filename: "russian_blue_01.jpg")
   
   siamese = User.create(name: "Siamese", email: "siamese@cat.com", password: "123456", password_confirmation: "123456",
-                        company: Faker::Company.name, college: Faker::Science.scientist)
+                        company: Faker::Company.name, college: Faker::Science.scientist, description: Faker::Lorem.sentence(3))
   3.times { siamese.photos.create }
   siamese.photos.first.file.attach(io: File.open("#{photo_dir}/siamese_01.jpg"), filename: "siamese_01.jpg")
   siamese.photos.second.file.attach(io: File.open("#{photo_dir}/siamese_02.jpg"), filename: "siamese_02.jpg")
