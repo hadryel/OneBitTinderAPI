b39d3db6f76e0b2fa0c3349d1adaf557cdd491d9
Daniel Moreto <dfmoreto@gmail.com>
2019-01-25 12:58:46 -0200

    Update ChatController to get Matches with messages

--- app/controllers/api/v1/chat_controller.rb
+++ app/controllers/api/v1/chat_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::ChatController < ApplicationController
   def index
+    page = params[:page] || 1
     @matches = current_user.matches.left_joins(:messages)
-                           .group(:id)
-                           .having("COUNT(messages.*) > 0")
-                           .paginate(page: params[:page] || 1)
+                                    .group(:id).having("COUNT(messages.*) > 0").order("created_at DESC")
     render "api/v1/matches/index"
   end
 end
