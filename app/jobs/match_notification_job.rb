8c9adf4fbb9443078d6f534509ae8ea5cda6a64b
Daniel Moreto <dfmoreto@gmail.com>
2019-01-25 13:03:19 -0200

    Update MatchNotificationJob to send notification via ActionCable

--- app/jobs/match_notification_job.rb
+++ app/jobs/match_notification_job.rb
@@ -2,7 +2,16 @@ class MatchNotificationJob < ApplicationJob
   queue_as :default
 
   def perform(match)
-    puts "Matcher = #{match.matcher.name}"
-    puts "Matchee = #{match.matchee.name}"
+    send_notification(match.matcher, match.matchee)
+    send_notification(match.matchee, match.matcher)
+  end
+
+
+  private
+
+
+  def send_notification(user, match_with)
+    MatchNotificationChannel.broadcast_to "match_notification_#{user.id}",
+      message: "Você deu um Match com #{match_with.name}"
   end
 end
