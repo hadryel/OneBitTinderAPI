0421a321cbdf116f11d610d7d9e115ec617ad3e7
Daniel Moreto <dfmoreto@gmail.com>
2019-01-25 13:01:50 -0200

    Update model Photo to set other photos as non-default

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,16 +3,22 @@ class Photo < ApplicationRecord
   has_one_attached :file
 
   after_create :check_user_default_photo
+  after_update :set_others_as_non_default, if: ->(photo) { photo.default? }
   
   def default!
     transaction do
-      self.user.photos.where.not(id: self.id).update(default: false)
+      set_others_as_non_default
       self.update(default: true)
     end
   end
 
 
   private
+
+
+  def set_others_as_non_default
+    self.user.photos.where.not(id: self.id).update_all(default: false)
+  end
 
 
   def check_user_default_photo
