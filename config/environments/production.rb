aff06e7f9ea0e6502e5be2caee0e9fbb400722c7
hadryel <hadryel.ruda@eadbox.com>
2019-07-25 18:15:28 -0300

    Fix to URL cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://hhobtapi.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://hhobt.herokuapp.com' ]
+  config.action_cable.allowed_request_origins = [ 'https://hhobt.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
